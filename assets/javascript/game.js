189af2d736f7c5af7b3bb2069ca10a520628ba37
VinTran <tranquillo.vinnie@gmail.com>
2019-02-06 19:45:08 -0500

    idk

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,9 +16,16 @@ var alreadyGuess = document.getElementById("alreadyGuessed");
 //stores and displays user guesses
 var x = 0;
 var guesses = Array();
+var movie = ["Jaws", "Gladiator", "Titanic"]
 
-
-
+window.onload = function() {
+chosenMovie = movie[Math.floor(Math.random() * movie.length)];
+word = chosenMovie[Math.floor(Math.random() * chosenMovie.length)];
+word.replace(/\s/g, "-");
+curWord.textContent = word;
+console.log(word);
+}
+//stores guessses and counts down number of guesses from 15
 result = document.onkeyup = function (event) {
 
     guesses[x] = event.key;
@@ -35,7 +42,19 @@ result = document.onkeyup = function (event) {
     if (guessRemain <= 0) {
         alert("YOU FAIL!!! REFRESH TO TRY AGAIN")
     }
+
+
+
+
+
+
+
+
+
 }
+
+
+
 
 guessRem.textContent = guessRemain;
 
